c0bdb7925f359a4b124334fa60f6de3229e7705c
unknown <kishormohite888@gmail.com>
2013-03-06 18:28:35 +0530

    solved problem with browser request

--- samurai.php
+++ samurai.php
@@ -1,30 +1,51 @@
 <?php
                                 
-                $address='127.0.0.1';
+               $address='127.0.0.1';
+				
                 $port=8079;
 				
-                $create=socket_create(AF_INET,SOCK_STREAM,SOL_TCP); 
+                if(($create=socket_create(AF_INET,SOCK_STREAM,SOL_TCP))===false) 
 				    //create socket
+                  {
+				  
+                        echo "error:".socket_strerror(socket_last_error())."\n";
                   
-               $bind=socket_bind($create,$address,$port);
+				  }
+				  
+               if(($bind=socket_bind($create,$address,$port))===false);  
 			       //bind a name to created socket
+                 {
+				 
+                        echo "error:".socket_strerror(socket_last_error())."\n";
+						
+                 }
                         socket_listen($create,$backlog=0);  
 						//listen for connection on socket
+						
    do{
 		if(($accept=socket_accept($create))===false) break;
-		//header("HTTP/1.0 404 Not Found");
 		//accept connection
+   
+		//header("HTTP/1.0 404 Not Found");
+		
 		$response="HTTP/1.1 200 OK \r\nDate: ".date('D, d M y H:i:s O')."\r\nServer: Samurai/1.0.0.1 \r\nAccept-Ranges: bytes \r\nConnection: close \r\nContent-Type: text/html; charset=UTF-8\r\n\n";
 		//standard http headers
+		
         socket_write($accept, $response, strlen($response));
+		
 		var_dump(headers_list());
-		if(headers_sent()===true) echo "true";
+		//dumps sent headers
+		
         $message="\n<h2><center>Welcome on Samurai 1.0.0.1</center></h2>";
+		
         socket_write($accept, $message, strlen($message));
+		
 	    socket_close($accept);
   
  }
 
   while(1);
+  
 socket_close($create);
+
 ?>
