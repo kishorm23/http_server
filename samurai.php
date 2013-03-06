0e9647510a7a968078b0bb6dd0145dbad39206b0
unknown <kishormohite888@gmail.com>
2013-03-06 18:17:03 +0530

    solved problem with browser request

--- samurai.php
+++ samurai.php
@@ -1,75 +1,30 @@
 <?php
-
+                                
                 $address='127.0.0.1';
-				
                 $port=8079;
 				
-                if(($create=socket_create(AF_INET,SOCK_STREAM,SOL_TCP))===false) 
+                $create=socket_create(AF_INET,SOCK_STREAM,SOL_TCP); 
 				    //create socket
-                  {
-				  
-                        echo "error:".socket_strerror(socket_last_error())."\n";
                   
-				  }
-               if(($bind=socket_bind($create,$address,$port))===false);  
+               $bind=socket_bind($create,$address,$port);
 			       //bind a name to created socket
-                 {
-				 
-                        echo "error:".socket_strerror(socket_last_error())."\n";
-						
-                 }
                         socket_listen($create,$backlog=0);  
 						//listen for connection on socket
    do{
-
 		if(($accept=socket_accept($create))===false) break;
+		//header("HTTP/1.0 404 Not Found");
 		//accept connection
-   
-		$response="HTTP/1.1 200 OK \n Date: ".date('D, d M y H:i:s O')."\nServer: Samurai/1.0.0.1 \nAccept-Ranges: bytes \nConnection: close \nContent-Type: text/html; charset=UTF-8\n";
+		$response="HTTP/1.1 200 OK \r\nDate: ".date('D, d M y H:i:s O')."\r\nServer: Samurai/1.0.0.1 \r\nAccept-Ranges: bytes \r\nConnection: close \r\nContent-Type: text/html; charset=UTF-8\r\n\n";
 		//standard http headers
-
-		socket_write($accept, $response, strlen($response));
-		
-		$m = "supported commands are QUIT and HELP.\n";
-		
-		socket_write($accept, $m, strlen($m));
-		
-		do {
-		 
-
-    		 if (($input = socket_read($accept, 2048, PHP_NORMAL_READ))===false) {
-                             break ;
-                           }
-						   
-        if (!$input === trim($input)) {
-            continue;
-        }
-		
-        if ($input === 'QUIT') {
-            break;
-        }
-
-		if($input === 'HELP'){
-		
-		     $help="QUIT to close existing connection.\n HELP to display this help message\n";
-			 
-			 socket_write($accept, $help, strlen($help));
-		}
-		
-        $back = "You said $input.\n";
-		
-        socket_write($accept, $back, strlen($back));
-		
-        echo "$input\n"; //echo on server
-		
-    } 
-	while (true);
-   
-   socket_close($accept);
+        socket_write($accept, $response, strlen($response));
+		var_dump(headers_list());
+		if(headers_sent()===true) echo "true";
+        $message="\n<h2><center>Welcome on Samurai 1.0.0.1</center></h2>";
+        socket_write($accept, $message, strlen($message));
+	    socket_close($accept);
   
  }
-while(1);
 
+  while(1);
 socket_close($create);
-
 ?>
