90db2ce7101189fbeb3d7b93c851fb4a52dfb563
Kishor Mohite <kishormohite888@gmail.com>
2013-03-07 18:34:35 +0530

    resolved browser request issue and some minor fixes

--- samurai.php
+++ samurai.php
@@ -6,15 +6,14 @@
                 $port=8079;
 				
                 if(($create=socket_create(AF_INET,SOCK_STREAM,SOL_TCP))===false) 
-				    //create socket
+				  //create socket
                   {
 				  
                         echo "error:".socket_strerror(socket_last_error())."\n";
-                  
-				  }
+                  }
 				  
-               if(($bind=socket_bind($create,$address,$port))===false);  
-			       //bind a name to created socket
+                if(($bind=socket_bind($create,$address,$port))===false);  
+			      //bind a name to created socket
                  {
 				 
                         echo "error:".socket_strerror(socket_last_error())."\n";
@@ -23,66 +22,69 @@
                         socket_listen($create,$backlog=10);  
 						//listen for connection on socket
 						
-   do{
-		if(($accept=socket_accept($create))===false) break;
-		//accept connection
+          do
+                {
+		        if(($accept=socket_accept($create))===false) break;
+		        //accept connection
       
-         if (($input = socket_read($accept, 2048, PHP_NORMAL_READ))===false) 
-		        {
+                        if (($input = socket_read($accept, 2048, PHP_NORMAL_READ))===false) 
+		         {
                              break ;
-                } 
-	    //reading request headers
+		         }
+	               //reading request headers
 						   
-        if (!$input === trim($input)) {
-            continue;
-        }
+                        if (!$input === trim($input)) 
+                        {
+                            continue;
+                        }
 		
-		$request=substr($input, 5, strpos($input, 'HTTP')-5);
-		//extracting filename from header
+	               $request=substr($input, 5, strpos($input, 'HTTP')-5);
+	              //extracting filename from header
 		
-		$request=urldecode($request);
-		//decoding if url contains any special characters
+		       $request=urldecode($request);
+		      //decoding if url contains any special characters
 		
-		//checking if page exists on server
-		if(file_exists($request))
-		{
+	               echo $request;
 		
-	    echo "error:".socket_strerror(socket_last_error())."\n";
+		      //checking if page exists on server
+		      if(file_exists($request))
+	 	      { 
 		
-		//header("HTTP/1.0 404 Not Found");
+		       //header("HTTP/1.0 404 Not Found");
 		
-		$response="HTTP/1.1 200 OK\r\nDate: ".date('D, d M y H:i:s O')."\r\nServer: samurai/1.0.0.2\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nContent-Length: 53\r\nKeep-Alive: timeout=5, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n\n";
-		//standard http headers
+		       $response="HTTP/1.1 200 OK\r\nDate: ".date('D, d M y H:i:s O')."\r\nServer: samurai/1.0.0.1\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nContent-Length: 53\r\nKeep-Alive: timeout=5, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n\n";
+		       //standard http headers
 		
-		var_dump(headers_list());
-		//dumps sent headers
+		       var_dump(headers_list());
+		       //dumps sent headers
 		
-        $file = file_get_contents($request, true);
-		//get contents of requested page
+                       $file = file_get_contents($request, true);
+		       //get contents of requested page
 		
-		$message=$response.$file;
+		       $message=$response.$file;
 		
-        socket_write($accept, $message, strlen($message));
-		}
+                       socket_write($accept, $message, strlen($message));
+		       }
 
-       //if file not exist		
-		else {
+                       //if file not exist		
+		       else 
+		       {
+		 
+		       $response="HTTP/1.1 404 Not Found\r\nDate: ".date('D, d M y H:i:s O')."\r\nServer: samurai/1.0.0.1\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nContent-Length: 53\r\nKeep-Alive: timeout=5, max=100\r\nContent-Type: text/html\r\n\n";
 		
-		$response="HTTP/1.1 404 Not Found\r\nDate: ".date('D, d M y H:i:s O')."\r\nServer: samurai/1.0.0.2\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nContent-Length: 53\r\nKeep-Alive: timeout=5, max=100\r\nContent-Type: text/html\r\n\n";
+		       $message="<h1>404 Not Found</h1><br><h4>Samurai/1.0.0.1<br>".date('D, d M y H:i:s O')."</h4>";
 		
-		$message="<h1>404 Not Found</h1><br><h4>Samurai/1.0.0.1<br>".date('D, d M y H:i:s O')."</h4>";
+		       $message=$response.$message;
 		
-		$message=$response.$message;
+		       socket_write($accept, $message, strlen($message));
 		
-		socket_write($accept, $message, strlen($message));
-		
-		}
+		      } 
+		      socket_close($accept);
+
+		  } 
 
         while(1);
-		
-	    socket_close($accept);
-
   
-socket_close($create);
+    socket_close($create);
 
 ?>
